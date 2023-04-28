dd461f938274782f8d0963bd9a50fdb301006b55
jeff <a866325272@gmail.com>
2023-04-28 21:10:00 +0800

    fix cloudbuild

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ cwb_token = os.getenv('CWB_TOKEN')
 access_token = os.getenv('ACCESS_TOKEN')
 secret = os.getenv('SECRET')
 openai_token = os.getenv('OPENAI_TOKEN')
-google_adc = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+
 log_path = os.getenv('LOG_PATH')
 import firestore
 openai.api_key = openai_token
