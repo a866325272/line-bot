3ad67f38f22be0a4c8256c24758b13d4a5acd17e
jeff <a866325272@gmail.com>
2023-07-25 14:36:48 +0800

    add typhoon feature

--- app.py
+++ app.py
@@ -633,9 +633,11 @@ def linebot():
                         reply_message("請輸入金額", tk, access_token)
             else:
                 if text == '雷達回波圖' or text == '雷達回波':
-                    reply_image(f'https://cwbopendata.s3.ap-northeast-1.amazonaws.com/MSC/O-A0058-003.png?{time.time_ns()}', tk, access_token)
+                    reply_image(f'https://cwbopendata.s3.ap-northeast-1.amazonaws.com/MSC/O-A0058-001.png?{time.time_ns()}', tk, access_token)
                 elif text == '衛星雲圖':
                     reply_image(f'https://cwbopendata.s3.ap-northeast-1.amazonaws.com/MSC/O-C0042-002.jpg?{time.time_ns()}', tk, access_token)
+                elif text == '颱風':
+                    reply_image(f'https://cwbopendata.s3.ap-northeast-1.amazonaws.com/MSC/O-C0032-002.jpg?{time.time_ns()}', tk, access_token)
                 elif text == '地震資訊' or text == '地震':
                     quake = earth_quake()                           # 爬取地震資訊
                     push_message(quake[0], ID, access_token)  # 傳送地震資訊 ( 用 push 方法，因為 reply 只能用一次 )
